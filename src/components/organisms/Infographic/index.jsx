1babae38c2555833dd98a567e38690b598e371da
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2021-12-06 14:17:21 +0700

    add some func

--- src/components/organisms/Infographic/index.jsx
+++ src/components/organisms/Infographic/index.jsx
@@ -3,15 +3,36 @@ import { useState } from 'react'
 import axios from 'axios'
 
 const Infographic = () => {
-  const [value, onChange] = useState(new Date());
-  const [values, onChanges] = useState(new Date());
-  const [searchOK, setsearchOK] = useState(false)
+  const [fromDate, onFromDate] = useState(new Date());
+  const [endDate, onEndDate] = useState(new Date());
 
-  const OnClickSearch = () => {
-    if ( searchOK === '' ) {
-      setsearchOK(true)
-    } else {
-      setsearchOK('Error')
+  const OnClickSearch = async (e) => {
+    if ( e._reactName === 'onClick' ) {
+      console.log(e)
+    } else if ( e._reactName === 'onKeyDown' ) {
+      if ( e.key === 'Enter' ) {
+        let NewFormData = new FormData()
+        NewFormData.append('username', document.querySelector('#data-username').value)
+        NewFormData.append('division', document.querySelector('#data-division').value)
+        NewFormData.append('from', fromDate)
+        NewFormData.append('end', endDate)
+        let config = {
+          url: 'http://localhost:3031/infographics/detect/params',
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${ JSON.parse(readCookie('dataUser')).result.token }`
+          },
+          data: NewFormData
+        }
+        
+        await axios(config).then(response => {
+          console.log(response)
+        }).catch(err => {
+          console.log(err)
+        })
+        
+      }
     }
   }
 
@@ -51,12 +72,26 @@ const Infographic = () => {
         <span className="title">Visitor Infographic</span>
 
         <div className="mb-3 row">
+          <label className="col-sm-2 col-form-label">username</label>
+          <div className="col-sm-6">
+            <input onKeyDown={ OnClickSearch } type="text" id="data-username" className="form-control input-custom" placeholder="username" />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
+          <label className="col-sm-2 col-form-label">division</label>
+          <div className="col-sm-6">
+            <input onKeyDown={ OnClickSearch } type="text" id="data-division" className="form-control input-custom" placeholder="division" />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
           <label className="col-sm-2 col-form-label">Periode Start</label>
           <div className="col-sm-2">
             <DatePicker
               className="custom-input-date"
-              onChange={onChange}
-              value={value}
+              onChange={onFromDate}
+              value={fromDate}
               maxDetail="month"
             />
           </div>
@@ -64,13 +99,13 @@ const Infographic = () => {
           <div className="col-sm-2">
             <DatePicker
               className="custom-input-date"
-              onChange={onChanges}
-              value={values}
+              onChange={onEndDate}
+              value={endDate}
               maxDetail="month"
             />
           </div>
           <div className="col-sm-2 text-end">
-            <button onClick={OnClickSearch} 
+            <button onClick={ OnClickSearch } 
             className="btn btn-warning">Search</button>
           </div>
         </div>
@@ -80,10 +115,10 @@ const Infographic = () => {
         <div className="title">Result</div>
             <div className="lists">
                 <div className="item-result d-flex">
-                  <div className="content" style={ searchOK ? { display: 'block' } : { display: 'none' }}>
+                  {/* <div className="content" style={ searchOK ? { display: 'block' } : { display: 'none' }}>
                     <img style={{marginLeft: "200px"}} src="/images/Infographic/Graphic.png" className="image" alt="" />
                     <img style={{marginLeft: "350px",marginTop: "50px"}}src="/images/Infographic/Chart.png" className="image" alt="" />
-                  </div>
+                  </div> */}
                 </div>
             </div>
         </div>
